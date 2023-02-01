ddb87a2e5e2a40f3c8a2a926a7f2401af066f5a5
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-01 09:22:21 +0000

    Add files via upload

--- CA117/Week 3/primecomps_031.py
+++ CA117/Week 3/primecomps_031.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 
-# Explation on code:
-# "i for i in range(2, n + 1)" will iterate from numbers 2 to n + 1, (2 to n is also acceptable.)
-# "if all(i % j != 0 for j in range(2, i) works as follows:
-
-# 1: for j in range(2, i) means j will iterate through numbers 2 to number "i".
-# 2: The "all()" method is a method that returns "True" if all conditions are met, False otherwise.
-# In this task, the conditions we want to meet is for all numbers between 2 to i to all have remainders when divisble.
-# This is in order to check for a prime number, and that is displayed with "i % j != 0"
-# So, if all(i % j != 0 for j in range(2, i)) will only append i to the list if all numbers that j iterates have remainders. 
-
 import sys
 
 inputs = sys.stdin.readlines()
-
 for n in inputs:
+	primes = []
 	n = int(n.strip())
-	primes = [i for i in range(2, n + 1) if all(i % j != 0 for j in range(2, i))]
+	for i in range(2, n + 1):
+		count = 0
+		for j in range(2, i):
+			if i % j == 0:
+				count += 1
+			else:
+				pass
+		if count == 0:
+			primes.append(i)
 	print(f'Primes: {primes}')
