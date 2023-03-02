74f891878c58bbdb3a22e863e3ced2b51b73ca58
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-03-02 15:38:08 +0000

    Update crossword_122.py

--- CA117/Bucketlist/crossword_122.py
+++ CA117/Bucketlist/crossword_122.py
@@ -1,31 +1,13 @@
-import sys 
+import sys
 
-first, second = sys.stdin.readline().strip().split()
-first, second = first.lower(), second.lower()
+first, second = sys.stdin.readline().lower().strip().split()
+letters = [l for l in first if l.isalpha() and l in second]
+firstLast, secondLast = first.rfind(letters[-1]), second.rfind(letters[-1])
 
-i = len(second) - 1
-
-while i >= 0:
-    if second[i] in first and ("a" <= second[i] <= "z"):
-        break
-    i -= 1
-    
-lastLetter, index = second[i], i
-i = len(first) - 1
-count = 0
-lastDots = (len(second) - 1) - index
-grid = []
-
-while i >= 0:
-    if first[i] != lastLetter or (first[i] == lastLetter and count == 1):
-        append = (("." * index) + first[i] + ("." * (lastDots)))
-        grid.append(append)
-    elif first[i] == lastLetter and count < 1:
-        count += 1
-        grid.append(second)
-    i -= 1
-    
-i = len(grid) - 1
-while i >= 0:
-    print(grid[i])
-    i -= 1
+i = 0
+while i < len(first):
+    if i != firstLast:
+        print(("." * secondLast) + first[i] + ("." * (len(second) - 1 - secondLast)))
+    else:
+        print(second)
+    i += 1
