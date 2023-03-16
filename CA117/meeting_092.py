94b9445f22417b3b94c2a9cce094715367366b30
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-03-16 20:37:03 +0000

    Delete meeting_092.py

--- CA117/meeting_092.py
+++
@@ -1,8 +0,0 @@
-class Meeting():
-	def __init__(self, hour, minute, duration):
-		self.hour = hour
-		self.minute = minute
-		self.duration = duration
-
-	def __str__(self):
-		return f'{self.hour:02d}:{self.minute:02d} ({self.duration} minutes)'
