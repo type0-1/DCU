25e47dcff741975eff98ee46d72108b03d608f0b
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-03-16 20:36:29 +0000

    Delete gaa_091.py

--- CA117/gaa_091.py
+++
@@ -1,7 +0,0 @@
-class Score():
-	def __init__(self, goals=0, points=0):
-		self.goals = goals
-		self.points = points
-
-	def __str__(self):
-		return "{} goal(s) and {} point(s)".format(self.goals, self.points)
