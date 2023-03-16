55e1387a73daab055bdcd237717a7457679f1238
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-03-16 20:36:12 +0000

    Delete contact_092.py

--- CA117/contact_092.py
+++
@@ -1,8 +0,0 @@
-class Contact():
-	def __init__(self, name, phone, email):
-		self.name = name
-		self.phone = phone
-		self.email = email
-
-	def __str__(self):
-		return f'{self.name} {self.phone} {self.email}'
