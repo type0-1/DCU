733f81ac98fb76b81915fb9cff3373e4d13c9a50
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-03-20 21:21:20 +0000

    Add files via upload

--- CA117/Week 9/classlist_092.py
+++ CA117/Week 9/classlist_092.py
@@ -1,32 +1,29 @@
 class Student(object):
-   def __init__(self, name, uid, modules, average=0):
-      self.name = name
-      self.uid = uid
-      self.modules = modules
-      self.average = average
+    def __init__(self, name, uid, modules, mark=0):
+        self.name = name
+        self.uid = uid
+        self.modules = modules
+        self.mark = mark
 
-   def getAverage(self):
-      self.average = round((self.modules[0][1] + self.modules[1][1]) / 2)
-      return self.average
+    def getAverage(self):
+        self.mark = round(sum(module[1] for module in self.modules) / len(self.modules))
+        return self.mark
 
-   def __str__(self):
-      self.modules = sorted(self.modules)
-      output = []
-      output.append(f'Name: {self.name}')
-      output.append(f'ID: {self.uid}')
-      output.append(f'Modules: {self.modules[0][0]}, {self.modules[1][0]}')
-      output.append(f'Average mark: {self.average}')
-      return "\n".join(output)
+    def __str__(self):
+        output = []
+        self.modules = sorted(self.modules)
+        output.append(f'Name: {self.name}')
+        output.append(f'ID: {self.uid}')
+        output.append(f'Modules: {", ".join([module[0] for module in self.modules])}')
+        output.append(f'Average mark: {self.getAverage()}')
+        return "\n".join(output)
 
 class Classlist(object):
-   def __init__(self):
-      self.dic = {}
+	def __init__(self):
+		self.dic = {}
 
-   def add(self, other):
-      self.dic[other.getAverage()] = other
+	def add(self, other):
+	   self.dic[other.uid] = other
 
-   def __str__(self):
-     output = []
-     for k, v in sorted(self.dic.items(), reverse=True):
-        output.append(f'{v}')
-     return "\n".join(output)
+	def __str__(self):
+	   return "\n".join([f'{v}' for k, v in sorted(self.dic.items(), reverse=True)])
