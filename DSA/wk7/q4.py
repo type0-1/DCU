b72e5cf87426aefe938a6c681b913956ec33fbe8
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-10-26 21:33:48 +0100

    Quick fix

--- DSA/wk7/q4.py
+++ DSA/wk7/q4.py
@@ -46,18 +46,10 @@ head = Node(1)
 n2 = Node(2)
 n3 = Node(3)
 n4 = Node(4)
-n5 = Node(5)
-n6 = Node(6)
-n7 = Node(7)
-n8 = Node(8)
 
 head.next = n2
 n2.next = n3
 n3.next = n4
-n4.next = n5
-n5.next = n6
-n6.next = n7
-n7.next = n8
 
 head = swap_pairs(head)
 
