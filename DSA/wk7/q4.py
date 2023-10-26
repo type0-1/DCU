bb5d0fb334f0ab345a1637c46bfe4156b34d251e
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-10-26 18:59:18 +0100

    Finished

--- DSA/wk7/q4.py
+++ DSA/wk7/q4.py
@@ -8,22 +8,42 @@ class Node:
 def swap_pairs(head):
 
     new_head = head.next
-    tmp = head
+    
+    prev = head
+    curr = head.next
 
-   
+    while curr:
+        
+        prev.next = curr.next
+        curr.next = prev
+        tmp = prev.next
+        prev = tmp
+
+        if tmp is None:
+            curr = tmp
+            break
+
+        curr = tmp.next
+
+        head.next = curr
+        head = curr
 
 
-	
+    
     return new_head
 
 head = Node(1)
 n2 = Node(2)
 n3 = Node(3)
 n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
 
 head.next = n2
 n2.next = n3 
 n3.next = n4
+n4.next = n5
+n5.next = n6
 
 head = swap_pairs(head)
 
