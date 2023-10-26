c9440b4e7570a88372116464f94ed1175eb162a0
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-10-26 21:25:23 +0100

    Fixed q4 solution

--- DSA/wk7/q4.py
+++ DSA/wk7/q4.py
@@ -8,28 +8,33 @@ class Node:
 def swap_pairs(head):
 
     new_head = head.next
-    
+
     prev = head
     curr = head.next
 
-    while curr:
-        
-        prev.next = curr.next
+    if new_head.next == None:
+
+        temp = curr.next
         curr.next = prev
-        tmp = prev.next
-        prev = tmp
+        prev.next = temp
+        
+        return new_head
 
-        if tmp is None:
-            curr = tmp
-            break
+    while curr:
 
-        curr = tmp.next
+        temp = curr.next
+        curr.next = prev
+        prev.next = temp
 
         head.next = curr
-        head = curr
+        head = prev
+        prev = temp
 
+        if temp is None:
+            return new_head
 
-    
+        curr = temp.next
+
     return new_head
 
 head = Node(1)
@@ -38,12 +43,16 @@ n3 = Node(3)
 n4 = Node(4)
 n5 = Node(5)
 n6 = Node(6)
+n7 = Node(7)
+n8 = Node(8)
 
 head.next = n2
-n2.next = n3 
+n2.next = n3
 n3.next = n4
 n4.next = n5
 n5.next = n6
+n6.next = n7
+n7.next = n8
 
 head = swap_pairs(head)
 
