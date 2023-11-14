1e9ef7b2b1ba647f78e49b1142b5eb02c18d407b
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-11-14 10:01:49 +0000

    Redone csv-select.py

--- CA116/Week 7/csv-select.py
+++ CA116/Week 7/csv-select.py
@@ -2,20 +2,86 @@
 
 import sys
 
-command = sys.argv[1]
-command = command.split("=")
-inputs = sys.stdin.readlines()
+# Part 1:
+
+command_line = sys.argv[1]
+
+find_header = 0
+
+header = field = ""
+
+while command_line[find_header] != "=":
+    find_header += 1
+
+header = command_line[:find_header]
+field = command_line[find_header + 1:]
+
+# Part 2:
+
+fields = input()
+
+header_pos = 0
+left_pos = 0
 i = 0
 
-while i < len(inputs) - 1:
-   location = inputs[0].strip()
-   location = location.split(",")
-   current = inputs[i].strip()
-   current = current.split(",")
-   j = 0
-   while j < len(location):
-      if location[j] == command[0]:
-         if current[j] == command[1]:
-            print(",".join(current))
-      j += 1
-   i += 1
+while i < len(fields):
+
+    if fields[i] == ",":
+
+        header_pos += 1
+
+        if fields[left_pos:i] == header:
+
+            i = len(fields) + 1
+
+        left_pos = i + 1
+
+    if i + 1 == len(fields):
+
+        header_pos += 1
+
+        i = len(fields) + 1
+
+    i += 1
+
+# Part 3:
+
+entry = input()
+
+while entry != "end":
+
+    if header_pos == 10:
+
+        if entry[-2:] == field:
+
+            print(entry)
+
+    else:
+
+        i = 0
+        left_pos = i
+        comma_count = 0
+
+        while i < len(entry):
+
+            if entry[i] == ",":
+
+                comma_count += 1
+
+                if comma_count == header_pos - 1:
+
+                    left_pos = i + 1
+
+                    i = len(entry) + 1
+
+            i += 1
+
+        right_pos = left_pos
+
+        while entry[right_pos] != ",":
+            right_pos += 1
+
+        if entry[left_pos:right_pos] == field:
+            print(entry)
+
+    entry = input()
